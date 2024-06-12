21d2e3d77bbef808806cd47e4caf62be0c1be48c
Lazkerz <Laziranurul@gmail.com>
2024-06-12 08:39:01 +0700

    feat: update add, index product

--- resources/js/Pages/Dashboard/Seller/Product/Edit.jsx
+++ resources/js/Pages/Dashboard/Seller/Product/Edit.jsx
@@ -1,34 +1,230 @@
+import React from 'react';
+import { Head, useForm, usePage } from '@inertiajs/react';
+import SellerLayout from '@/Layouts/SellerLayout';
+import InputLabel from '@/Components/atom/InputLabel';
+import TextInput from '@/Components/atom/TextInput';
+import Select from '@/Components/atom/Select';
+import InputError from '@/Components/atom/InputError';
+import PrimaryButton from '@/Components/atom/PrimaryButton';
+import Checkbox from '@/Components/Checkbox';
 
+const categoryOptions = [
+    { value: 1, label: "Padi" },
+    { value: 2, label: "Jagung" },
+    { value: 3, label: "Cabai" }
+];
+
+const unitOptions = [
+    { value: 1, label: "Piece" },
+    { value: 2, label: "Kg" },
+    { value: 3, label: "Ton" },
+    { value: 4, label: "Box" }
+];
 
 export default function Edit() {
-
-    const [ data, setData ] = useState({
+    const { data, setData, post, processing, errors } = useForm({
         name: '',
-        shop: '',
         category: '',
         unit: '',
-        description:'',
+        description: '',
         price: '',
         stock: '',
         image: null,
-    })
+        tag: [],
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        console.log(data);
+
+        const formData = new FormData();
+        formData.append('name', data.name);
+        formData.append('category', data.category);
+        formData.append('unit', data.unit);
+        formData.append('description', data.description);
+        formData.append('price', data.price);
+        formData.append('stock', data.stock);
+        formData.append('image', data.image);
+        formData.append('tag', JSON.stringify(data.tag)); // Convert tag array to JSON string
+
+        post(route('seller.product.store'), {
+            data: formData,
+            onSuccess: () => {
+                route('dashboard.seller.product.index');
+            },
+        });
+    };
+
+    const handleFileChange = (e) => {
+        setData('image', e.target.files[0]);
+    };
+
+    const handleCheckboxChange = (e) => {
+        const { value, checked } = e.target;
+        const tags = data.tag;
+        if (checked) {
+            setData('tag', [...tags, value]);
+        } else {
+            setData('tag', tags.filter(tag => tag !== value));
+        }
+    };
+
     return (
-        <>
-        <SellerLayout>   
-        <div className='w-full bg-white min-h-screen py-15 md:py-10 px-16 flex justify-center items-center flex flex-col'>
-                <div className='flex flex-col mt-20 md:mt-0'>
-                    <h1 className='text-center bg-amber-300 text-2xl md:text-4xl font-semibold mb-6 w-fit mx-auto'>Tambah Produk</h1>
-                </div>
-                <form className="flex flex-col items-center">
-                    <FormCreate data={data} setData={setData}/>
-                    <div className="flex items-center justify-center mt-4">
-                        <PrimaryButton className="w-full text-center" disabled={processing}>
+        <SellerLayout>
+            <Head title="Create Product" />
+            <div className="min-h-screen flex flex-col sm:justify-center items-center my-10 pt-10 bg-white">
+                <h1 className="text-center bg-amber-300 text-xl md:text-2xl font-semibold mb-6 w-fit mx-auto">Tambah Produk di Toko Anda</h1>
+                <form onSubmit={submit} className="flex flex-col items-center w-full">
+                    <div className='md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="name" value="Nama Produk" />
+                        <TextInput
+                            id="name"
+                            type="text"
+                            name="name"
+                            value={data.name}
+                            className="mt-2 block w-full"
+                            autoComplete="name"
+                            isFocused={true}
+                            onChange={(e) => setData('name', e.target.value)}
+                            required
+                        />
+                        <InputError message={errors.name} className="mt-2" />
+                    </div>
+
+                    <div className='w-[220px] md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="category" value="Kategori" />
+                        <Select
+                            id="category"
+                            name="category"
+                            value={data.category}
+                            className="mt-2 block w-full"
+                            options={categoryOptions}
+                            placeholder="Pilih Kategori Produk"
+                            onChange={(e) => setData('category', e.target.value)}
+                            required
+                        />
+                        <InputError message={errors.category} className="mt-2" />
+                    </div>
+
+                    <div className='w-[220px] md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="unit" value="Unit" />
+                        <Select
+                            id="unit"
+                            name="unit"
+                            value={data.unit}
+                            className="mt-2 block w-full"
+                            options={unitOptions}
+                            placeholder="Pilih Jenis Unit"
+                            onChange={(e) => setData('unit', e.target.value)}
+                            required
+                        />
+                        <InputError message={errors.unit} className="mt-2" />
+                    </div>
+
+                    <div className='md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="description" value="Deskripsi" />
+                        <TextInput
+                            id="description"
+                            type="text"
+                            name="description"
+                            value={data.description}
+                            className="mt-2 block w-full"
+                            autoComplete="description"
+                            isFocused={true}
+                            onChange={(e) => setData('description', e.target.value)}
+                        />
+                        <InputError message={errors.description} className="mt-2" />
+                    </div>
+
+                    <div className='md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="price" value="Harga" />
+                        <TextInput
+                            id="price"
+                            name="price"
+                            type="number"
+                            value={data.price}
+                            className="mt-2 block w-full"
+                            autoComplete="price"
+                            isFocused={true}
+                            onChange={(e) => setData('price', e.target.value)}
+                            required
+                        />
+                        <InputError message={errors.price} className="mt-2" />
+                    </div>
+
+                    <div className='md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="stock" value="Stok Produk" />
+                        <TextInput
+                            id="stock"
+                            name="stock"
+                            type="number"
+                            value={data.stock}
+                            className="mt-2 block w-full"
+                            autoComplete="stock"
+                            isFocused={true}
+                            onChange={(e) => setData('stock', e.target.value)}
+                            required
+                        />
+                        <InputError message={errors.stock} className="mt-2" />
+                    </div>
+
+                    <div className='w-[220px] md:w-1/4 mt-4'>
+                        <InputLabel htmlFor="image" value="Gambar Produk"/>
+                        <TextInput
+                            id="image"
+                            type="file"
+                            name="image"
+                            accept="image/*"
+                            className="mt-1 w-full p-2.5 focus:outline file:rounded-lg file:border-0 file:bg-amber-500 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-white hover:file:bg-amber-400"
+                            autoComplete="image"
+                            onChange={handleFileChange}
+                        />
+                        <InputError message={errors.image} className="mt-2" />
+                    </div>
+
+                    <div className='justify-start w-1/2 md:w-1/4 mt-4'>
+                        <InputLabel value="Tag Produk" />
+                        <div className="flex items-center mt-2">
+                            <Checkbox
+                                id="tag1"
+                                name="tag"
+                                value="1"
+                                className="mr-2"
+                                onChange={handleCheckboxChange}
+                            />
+                            <InputLabel htmlFor="tag1" value="Segar" />
+                        </div>
+                        <div className="flex items-center mt-2">
+                            <Checkbox
+                                id="tag2"
+                                name="tag"
+                                value="2"
+                                className="mr-2"
+                                onChange={handleCheckboxChange}
+                            />
+                            <InputLabel htmlFor="tag2" value="Terbaik" />
+                        </div>
+                        <div className="flex items-center mt-2">
+                            <Checkbox
+                                id="tag3"
+                                name="tag"
+                                value="3"
+                                className="mr-2"
+                                onChange={handleCheckboxChange}
+                            />
+                            <InputLabel htmlFor="tag3" value="Enak" />
+                        </div>
+                        <InputError message={errors.tag} className="mt-2" />
+                    </div>
+
+                    <div className="flex items-center justify-center mt-4 text-center">
+                        <PrimaryButton className="w-full" disabled={processing}>
                             Buat
                         </PrimaryButton>
                     </div>
-                </form>    
+                </form>
             </div>
         </SellerLayout>
-        </>
-    ); 
+    );
 }
--- resources/js/Pages/Dashboard/Seller/Product/Index.jsx
+++ resources/js/Pages/Dashboard/Seller/Product/Index.jsx
@@ -1,69 +1,35 @@
+import React from 'react';
+import { Head, usePage, Link } from '@inertiajs/react';
 import SellerLayout from "@/Layouts/SellerLayout";
 
 export default function Product() {
+    const { products } = usePage().props;
+
     return (
-        <>
         <SellerLayout>
-        <div className='min-h-screen flex flex-col sm:justify-center items-center my-10 sm:pt-0 bg-white'>
+            <Head title="Daftar Produk" />
+            <div className='min-h-screen flex flex-col sm:justify-center items-center my-10 sm:pt-0 bg-white'>
                 <div className='flex flex-col mt-20 md:mt-0'>
                     <h1 className='text-center bg-amber-300 text-2xl md:text-4xl font-semibold mb-6 w-fit mx-auto'>Daftar Produk Anda</h1>
                     <p className='text-center font-medium text-sm md:text-md'>Tambah Produk Anda dengan mudah.</p>
                     <a href="product/create" className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mx-auto mt-5 hover:bg-amber-400 cursor-pointer">Tambah</a>
                 </div>
-                    <ul className='flex flex-col md:flex-row flex-wrap gap-5 justify-center items-center m-10 overflow-hidden'>
-                    <li className='border-2 border-dashed border-amber-300 p-3 rounded-lg flex flex-col justify-centershadow-xl w-[250px] h-[300px] overflow-hidden'>
-                        <img src="../../images/beras.jpg" alt="step1" className="w-[250px] h-[120px] object-cover" />
-                        <div className="flex flex-row justify-between my-2">
-                        <p className='font-semibold'>Beras</p>
-                        <p className="font-medium text-amber-500">Kategori</p>
-                        </div>
-                        <p className="font-medium text-gray-500 mb-1">2 Ton Tersedia</p>
-                        <p className="font-bold text-lime-600">Rp. 100.000</p>
-                        <button className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mt-3 hover:bg-amber-400">Edit</button>
-                    </li>
-                    <li className='border-2 border-dashed border-amber-300 p-3 rounded-lg flex flex-col justify-centershadow-xl w-[250px] h-[300px] overflow-hidden'>
-                        <img src="../../images/beras.jpg" alt="step1" className="w-[250px] h-[120px] object-cover" />
-                        <div className="flex flex-row justify-between my-2">
-                        <p className='font-semibold'>Beras</p>
-                        <p className="font-medium text-amber-500">Kategori</p>
-                        </div>
-                        <p className="font-medium text-gray-500 mb-1">2 Ton Tersedia</p>
-                        <p className="font-bold text-lime-600">Rp. 100.000</p>
-                        <button className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mt-3 hover:bg-amber-400">Edit</button>
-                    </li>
-                    <li className='border-2 border-dashed border-amber-300 p-3 rounded-lg flex flex-col justify-centershadow-xl w-[250px] h-[300px] overflow-hidden'>
-                        <img src="../../images/beras.jpg" alt="step1" className="w-[250px] h-[120px] object-cover" />
-                        <div className="flex flex-row justify-between my-2">
-                        <p className='font-semibold'>Beras</p>
-                        <p className="font-medium text-amber-500">Kategori</p>
-                        </div>
-                        <p className="font-medium text-gray-500 mb-1">2 Ton Tersedia</p>
-                        <p className="font-bold text-lime-600">Rp. 100.000</p>
-                        <button className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mt-3 hover:bg-amber-400">Edit</button>
-                    </li>
-                    <li className='border-2 border-dashed border-amber-300 p-3 rounded-lg flex flex-col justify-centershadow-xl w-[250px] h-[300px] overflow-hidden'>
-                        <img src="../../images/beras.jpg" alt="step1" className="w-[250px] h-[120px] object-cover" />
-                        <div className="flex flex-row justify-between my-2">
-                        <p className='font-semibold'>Beras</p>
-                        <p className="font-medium text-amber-500">Kategori</p>
-                        </div>
-                        <p className="font-medium text-gray-500 mb-1">2 Ton Tersedia</p>
-                        <p className="font-bold text-lime-600">Rp. 100.000</p>
-                        <button className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mt-3 hover:bg-amber-400">Edit</button>
-                    </li>
-                    <li className='border-2 border-dashed border-amber-300 p-3 rounded-lg flex flex-col justify-centershadow-xl w-[250px] h-[300px] overflow-hidden'>
-                        <img src="../../images/beras.jpg" alt="step1" className="w-[250px] h-[120px] object-cover" />
-                        <div className="flex flex-row justify-between my-2">
-                        <p className='font-semibold'>Beras</p>
-                        <p className="font-medium text-amber-500">Kategori</p>
-                        </div>
-                        <p className="font-medium text-gray-500 mb-1">2 Ton Tersedia</p>
-                        <p className="font-bold text-lime-600">Rp. 100.000</p>
-                        <button href="dashboard/seller/product/edit"className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mt-3 hover:bg-amber-400">Edit</button>
-                    </li>
-                    </ul>       
+                <ul className='flex flex-col md:flex-row flex-wrap gap-5 justify-center items-center m-10 overflow-hidden'>
+                    {products.map(product => (
+                        <li key={product.id} className='border-2 border-dashed border-amber-300 p-3 rounded-lg flex flex-col justify-center shadow-xl w-[250px] h-[300px] overflow-hidden'>
+                            <img src={product.image_url || "../../images/beras.jpg"} alt={product.name} className="w-[250px] h-[120px] object-cover" />
+                            <div className="flex flex-col justify-between my-2">
+                                <p className='font-semibold'>{product.name}</p>
+                                <p className="font-medium text-amber-500">{product.tags.map(tag => tag.name).join(', ')}</p>
+                                <p className="font-medium text-amber-500">{product.category?.name}</p>
+                            </div>
+                            <p className="font-medium text-gray-500 mb-1">{product.stock} {product.unit}</p>
+                            <p className="font-bold text-lime-600">Rp. {product.price}</p>
+                            <Link href={`dashboard/seller/product/edit/${product.id}`} className="bg-amber-500 px-4 py-2 rounded-lg text-white text-base font-medium text-center w-fit mt-3 hover:bg-amber-400">Edit</Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </SellerLayout>
-        </>
     );
 }
