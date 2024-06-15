b10279e6d0abeaf015a60b9db7fb4d4c229a92a2
Rezha Jamil <rezha.jn@gmail.com>
2024-06-16 00:19:51 +0700

    fix:produk page bug,image url

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/tentang-kami', [HomeController::class, 'about'])->name('tentang-kam
 
 
 // Route::get('user/change-role', [UserController::class, 'changeRole'])->name('change-role');
-Route::get('user/change-role?role=Seller&route=seller.shop.index', [UserController::class, 'changeRole'])->name('change-seller');
+Route::get('user/change-role', [UserController::class, 'changeRole'])->name('change-seller');
 
 
 Route::middleware('auth')->group(function () {
